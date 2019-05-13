4135402fa8bafc4ec0035481c5ef7c2c774712be
Josh Nelsson-Smith <joshua.nelsson-smith@cbussuper.com.au>
2019-05-13 13:50:17 +1000

    Adds input styling

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PWA To-do List</h1>
+        <p>A simple to-do list PWA built using React</p>
       </header>
+      <main>
+        <div class="todo__container">
+          <input placeholder="Add to-do"></input>
+        </div>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
